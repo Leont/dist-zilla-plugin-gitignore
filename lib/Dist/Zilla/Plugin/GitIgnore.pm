eee92983683e2b95f558a0c0cd619792835f4436
Leon Timmermans <fawaka@gmail.com>
2025-07-20 19:45:29 +0200

    Add a positive override for .gitignore itself to the .gitignore

--- lib/Dist/Zilla/Plugin/GitIgnore.pm
+++ lib/Dist/Zilla/Plugin/GitIgnore.pm
@@ -31,6 +31,12 @@ has extras => (
 	},
 );
 
+has skip_gitignore_override => (
+	is      => 'ro',
+	isa     => Bool,
+	default => !!1,
+);
+
 has skip_defaults => (
 	is      => 'ro',
 	isa     => Bool,
@@ -43,6 +49,8 @@ sub gather_files($self) {
 		my $name = $self->zilla->name;
 		push @patterns, '/.build/', "/$name-*/", "/$name-*.tar.gz";
 	}
+	push @patterns, '!/.gitignore' if not $self->skip_gitignore_override;
+
 	push @patterns, $self->extras;
 
 	my $payload = join '', map { "$_\n" } @patterns;
@@ -67,7 +75,7 @@ In your profile.ini
 
 =head1 DESCRIPTION
 
-This is a minting plugin to add a C<.gitignore> file. By default it ignores only two things that L<Dist::Zilla> itself generates: the F<.build/> directory, and any directory or C<.tar.gz> file starting with C<"$dist_name-"> (e.g. the tarball that C<Dist::Zilla> produces). More patterns can be added using the C<extra> argument.
+This is a minting plugin to add a C<.gitignore> file. By default it ignores only two things that L<Dist::Zilla> itself generates: the F<.build/> directory, and any directory or C<.tar.gz> file starting with C<"$dist_name-"> (e.g. the tarball that C<Dist::Zilla> produces). More patterns can be added using the C<extra> argument. It will also add an override to never ignore the C<.gitignore> file itself (in case users have such a configuration).
 
 =attr extras
 
@@ -80,3 +88,7 @@ This sets the filename. It defaults to F<.gitignore> and probably shouldn't be c
 =attr skip_defaults
 
 If enabled, it won't add any default patterns. This is disabled by default.
+
+=attr skip_gitignore_override
+
+If enabled, it won't add skip the .gitignore override to the patterns. You're probably better off leaving it as it is.
