f8f98043bf1055a6096c6a15444f5ebecb341d76
Leon Timmermans <fawaka@gmail.com>
2025-07-20 18:01:52 +0200

    Initial version

---
+++ lib/Dist/Zilla/Plugin/Changes.pm
@@ -0,0 +1,71 @@
+package Dist::Zilla::Plugin::Changes;
+
+use Moose;
+with qw/Dist::Zilla::Role::FileGatherer/;
+use MooseX::Types::Moose qw/Str Int/;
+
+use experimental 'signatures';
+
+use namespace::autoclean;
+
+has filename => (
+	is      => 'ro',
+	isa     => Str,
+	default => 'Changes',
+);
+
+has initial => (
+	is        => 'ro',
+	isa       => Str,
+	predicate => 'has_initial',
+);
+
+has indent => (
+	is        => 'ro',
+	isa       => Int,
+	default   => 10,   # [NextRelease]
+);
+
+sub gather_files($self) {
+	my $header = "Revision history for " . $self->zilla->name;
+	my @lines = ($header, '', '{{$NEXT}}');
+
+	if ($self->has_initial) {
+		my $indent = ' ' x $self->indent;
+		push @lines, $indent . $self->initial
+	}
+
+	my $payload = join '', map { "$_\n" } @lines;
+	$self->add_file(Dist::Zilla::File::InMemory->new(
+		name    => $self->filename,
+		content => $payload,
+	));
+
+	return;
+}
+
+1;
+
+# ABSTRACT: Create a changes file
+
+=head1 SYNOPSIS
+
+In your profile.ini
+
+ [Changes]
+
+=head1 DESCRIPTION
+
+This is a minting plugin to add a changelog file, meant to be used together with L<NextRelease|https://metacpan.org/pod/Dist::Zilla::Plugin::NextRelease>.
+
+=attr filename
+
+This sets the filename of the changelog. It defaults to C<Changes>.
+
+=attr initial
+
+This is default entry for the initial version. This could be something like C<Initial release>. If not set, no such line is added.
+
+=attr indent
+
+This is the indentation used for the initial line
--- lib/Dist/Zilla/Plugin/GitIgnore.pm
+++ lib/Dist/Zilla/Plugin/GitIgnore.pm
@@ -1,6 +1,69 @@
 package Dist::Zilla::Plugin::GitIgnore;
 
-use strict;
-use warnings;
+use Moose;
+with qw/Dist::Zilla::Role::FileGatherer/;
+use MooseX::Types::Moose qw/ArrayRef Str/;
+
+use experimental 'signatures';
+
+use namespace::autoclean;
+
+sub mvp_multivalue_args($class) {
+	return 'extras';
+}
+
+sub mvp_aliases($class) {
+	return { extra => 'extras' };
+}
+
+has filename => (
+	is => 'ro',
+	isa => Str,
+	default => '.gitignore',
+);
+
+has extras => (
+	isa     => ArrayRef[Str],
+	traits  => ['Array'],
+	default => sub { [] },
+	handles => {
+		extras => 'elements',
+	},
+);
+
+sub gather_files($self) {
+	my @patterns = '.build';
+	push @patterns, $self->zilla->name . "-*";
+	push @patterns, $self->extras;
+	my $payload = join '', map { "$_\n" } @patterns;
+
+	$self->add_file(Dist::Zilla::File::InMemory->new(
+		name => $self->filename,
+		content => $payload,
+	));
+
+	return;
+}
 
 1;
+
+# ABSTRACT: Add .gitignore during minting
+
+=head1 SYNOPSIS
+
+In your profile.ini
+
+ [GitIgnore]
+ extra = *.swp
+
+=head1 DESCRIPTION
+
+This is a minting plugin to add a C<.gitignore> file. By default it ignores only two things that L<Dist::Zilla> itself generates: the F<.build> directory, and any file starting with C<"$dist_name-"> (e.g. the tarball that C<Dist::Zilla> produces). More patterns can be added using the C<extra> argument.
+
+=attr extras
+
+This is the list of extra patterns for the gitignore file.
+
+=attr filename
+
+This sets the filename. It defaults to F<.gitignore> and probably shouldn't be changed.
